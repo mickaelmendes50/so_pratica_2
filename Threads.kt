84e955732a6a5092f852aa4cdcc22868dba4b0d5
Mesquita <mesquita@aospa.co>
2026-01-30 20:57:56 -0300

    Implementado starter de threads
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- Threads.kt
+++ Threads.kt
@@ -1,7 +1,26 @@
+import kotlin.concurrent.thread
+
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("Uso: kotlin programa.kt <num_threads>")
         return
     }
-    val n = args[0].toInt() 
+    val n = args[0].toInt()
+
+    println("Iniciando $n threads...")
+
+    val threads = mutableListOf<Thread>()
+    for (i in 0 until n) {
+        val t = thread(start = true, name = "Worker-$i") {
+            println("Thread ${Thread.currentThread().name}")
+            var counter = 0L
+            while (true) {
+                counter += 1
+                Thread.sleep(10)
+            }
+        }
+        threads.add(t)
+    }
+
+    threads.forEach { it.join() }
 }
